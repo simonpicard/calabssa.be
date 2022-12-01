a77cd26a30c60d21267703c5045e45e169211def
Simon Picard <simonmyway@Simons-MacBook-Pro.local>
2022-12-01 14:31:39 +0100

    about

--- src/pages/about-page.js
+++ src/pages/about-page.js
@@ -6,15 +6,17 @@ export default function AboutPage() {
     AboutData.forEach((value, key) => { console.log(value, key) });
 
     return (
-        <div id="about-page" className="block justify-center divide-solid divide-y divide-slate-900/10 text-[#334155] items-center">
+        <div id="about-page" className="block divide-solid divide-y divide-slate-900/10 text-[#334155] items-center">
             {
                 AboutData.map((value, key) => {
                     return (
                         <details key={key} className="pt-4 pb-2">
-                            <summary className="cursor-pointer font-semibold mb-2">
+                            <summary className="cursor-pointer font-semibold pb-2">
                                 {value.q}
                             </summary>
-                            {value.a}
+                            <div className="mb-2 text-justify">
+                                {value.a}
+                            </div>
                         </details>
                     );
                 })
